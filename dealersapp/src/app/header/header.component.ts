0a9f55bf0c40c9d22aa9bc6ba3afeb68e4670a1d
kiranpaturi <kiran_p@k3informatics.com>
2017-03-01 19:29:28 +0530

    added code

--- dealersapp/src/app/header/header.component.ts
+++ dealersapp/src/app/header/header.component.ts
@@ -4,9 +4,27 @@ import {Component} from "@angular/core";
 
 @Component({
     selector:"app-header",
-    template:"<h1>I am the header</h1>"
+    templateUrl:"./header.component.html"
+    // template:"<h1>I am the header</h1>"
 })
 
 export class HeaderComponent{
+ //public member.
+ header={
+     brandName:"Product Deals"
+ };
+ showLink:boolean
 
+ clickMe(){
+     if(this.showLink ==true){
+         this.showLink=false;
+     }
+     else{
+         this.showLink =true;
+     }
+     console.log("Hey I got clicked");
+ }
+ handleKeyPress(){
+     console.log("Hey You are entering data");
+ }
 }

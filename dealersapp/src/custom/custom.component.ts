5620a384b527b264f995cb5ad7d6b7e0b2c9b4ce
padma <padmab@k3informatics.com>
2017-03-02 19:38:36 +0530

    added code

--- dealersapp/src/custom/custom.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'custom-root',
-  template:"<h1>Welcome to custom module<h1>"
-})
-export class CustomComponent {
-  title = 'Welcome to angular';
-}
